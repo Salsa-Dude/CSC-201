818a55c747e396b388c290dd644641396345bbf7
Joseph Arias <arias.joseph.a@gmail.com>
2020-10-04 17:24:53 -0400

    remove instruction comments

--- Mod3-Java-Programming/problem-1-highschool-locker/LockerDemo.java
+++ Mod3-Java-Programming/problem-1-highschool-locker/LockerDemo.java
@@ -1,12 +1,4 @@
 
-/*
- * A locker for a high school student has a locker number, a student name assigned to it, a number of books stored inside the locker, and also a combination lock. The lock is constructed with a combination that has 3 numbers between 0 and 39. The lock is opened if the user turns the lock to the right to the first combination number, and then left to the second combination number, and then right to third combination number. When the dial is reset it will then be pointing to 0.
- * 
-Create a class to represent the locker and another class to represent the combination lock. The Locker class will include an attribute that is of type CombinationLock. Each class must include a constructor with no input argument and also a constructor that requires input arguments for all attributes. Each class must include the appropriate set and get methods.
-
- * 
- */
-
 
 public class LockerDemo {
 
